2815f265e985ee6dfdd486d01c3e35275e97cfaa
nmiesmer <nic.miesmer@segmint.com>
2018-10-21 21:44:04 -0400

    showing list of items on start

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,78 @@ connection.connect(function(err) {
   if (err) {
       throw err;
     }
-
-    console.log("Hello Connection");
-  connection.end();
+    console.log("hello");
+    showItems();
+  //   console.log("Hello Connection");
+  // connection.end();
 });
+
+
+var showItems =function () {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+
+            choiceArray.push(results[i].product_name + " $" +  results[i].price + " Remaining: " + results[i].stock_quantity);
+            }
+            return choiceArray;
+          },
+          message: "What auction would you like to place a bid in?"
+        },
+        {
+          name: "bid",
+          type: "input",
+          message: "How much would you like to bid?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if bid was high enough
+        if (chosenItem.highest_bid < parseInt(answer.bid)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE auctions SET ? WHERE ?",
+            [
+              {
+                highest_bid: answer.bid
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              start();
+            }
+          );
+        }
+        else {
+          // bid wasn't high enough, so apologize and start over
+          console.log("Your bid was too low. Try again...");
+          start();
+        }
+      });
+  });
+}
+
+
+
+
+
---
+++ scratchsql.sql
@@ -0,0 +1,1 @@
+select * from bamazon.products;
