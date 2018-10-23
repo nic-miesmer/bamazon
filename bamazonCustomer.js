eae146b0d6967295f3795011be41abec8c3914bc
nmiesmer <nic.miesmer@segmint.com>
2018-10-22 23:49:46 -0400

    mostly fixed up basic version, need to retun cost of products bought

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "",
+  password: "nsmdark10o",
   database: "bamazon"
 });
 
@@ -15,7 +15,6 @@ connection.connect(function(err) {
   if (err) {
       throw err;
     }
-    console.log("hello");
     showItems();
   //   console.log("Hello Connection");
   // connection.end();
@@ -24,7 +23,7 @@ connection.connect(function(err) {
 
 var showItems =function () {
   // query the database for all items being auctioned
-  connection.query("SELECT * FROM products", function(err, results) {
+  connection.query("SELECT * FROM products order by item_id ASC", function(err, results) {
     if (err) throw err;
     // once you have the items, prompt the user for which they'd like to bid on
     inquirer
@@ -34,59 +33,70 @@ var showItems =function () {
           type: "rawlist",
           choices: function() {
             var choiceArray = [];
+            console.log(results)
+            console.log(results.length)
             for (var i = 0; i < results.length; i++) {
+              console.log("ID: " + results[i].item_id + " " + results[i].product_name + " $" +  results[i].price + " Remaining: " + results[i].stock_quantity)
 
-            choiceArray.push(results[i].product_name + " $" +  results[i].price + " Remaining: " + results[i].stock_quantity);
+              var item_id = results[i].item_id;
+            choiceArray.push(item_id.toString());
+
+            // choiceArray.push(results[i].product_name + " $" +  results[i].price + " Remaining: " + results[i].stock_quantity);
+
             }
             return choiceArray;
           },
-          message: "What auction would you like to place a bid in?"
+          message: "What item would you like to buy?"
         },
         {
-          name: "bid",
+          name: "toBuy",
           type: "input",
-          message: "How much would you like to bid?"
+          message: "How many would you like to buy?"
         }
       ])
       .then(function(answer) {
         // get the information of the chosen item
         var chosenItem;
+        // console.log("Customer's choice: " + answer.choice);
         for (var i = 0; i < results.length; i++) {
-          if (results[i].product_name === answer.choice) {
+          if (results[i].item_id.toString() === answer.choice) {
             chosenItem = results[i];
           }
+      
         }
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
+        // determine if enough items
+        if (chosenItem.stock_quantity > answer.toBuy) {
           // bid was high enough, so update db, let the user know, and start over
+          console.log(answer.bid);
+          var newStock = chosenItem.stock_quantity - answer.toBuy
           connection.query(
-            "UPDATE auctions SET ? WHERE ?",
+            "UPDATE products SET ? WHERE ?",
             [
               {
-                highest_bid: answer.bid
+                //change to products table
+                stock_quantity: newStock
               },
               {
-                id: chosenItem.id
+                item_id: chosenItem.item_id
               }
             ],
             function(error) {
-              if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
+              if (error){ throw err;}
+              console.log("Items bought!");
+
             }
+            
           );
+
+          console.log("Item bought")
+          connection.end();
         }
         else {
           // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
+          console.log("Not enough inventory. Try again...");
+            connection.end();
         }
       });
   });
 }
-
-
-
-
-
